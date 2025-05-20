e931ee85807b9660a73430ea963f03e51a95a9ca
Mayaomoto <mayaomoto@gmail.com>
2025-05-20 16:58:11 -0300

    insere pergunta incompleto

--- test1.c
+++ test1.c
@@ -9,8 +9,8 @@ diminuir dinamicamen*/
 
 typedef struct 
 {
-    char enunciado[31];
-    char alternativa[21];
+    char enunciado[71];
+    char alternativa_escrita[4][21];
     char alternativa_correta;
     char nivel_dificuldade[21];
 
@@ -24,10 +24,44 @@ void leString(char texto[], int tam)
     setbuf(stdin,NULL);
 
 }
+Pergunta* inserePergunta(Pergunta* lista, int *totalPergunta ){
 
-int main(){
+    Pergunta nova;
 
+    printf("Digite o enunciado da pergunta: \n");
+    leString(lista->enunciado,71);
+
+
+    for (int i = 0; i < 4; i++)//sao 4 opções de alternativa 
+    {
+        printf("Digite a alternativa %c:" ,'A'+ i);//recebe a alternativa e armazena entre as 4 
+        leString(nova.alternativa_escrita,21);
+
+    }
     
+    printf("Digite a alternativa correta: (A, B, C ou D)\n");
+    scanf("%c",nova.alternativa_correta);
+    getchar();
+
+    printf("Digite o nivel de dificuldade: (muito dificil, dificil, medio, facil, muito facil)\n");
+    leString(nova.nivel_dificuldade,sizeof(nova.nivel_dificuldade));
+
+    lista = realloc(lista,(*totalPergunta + 1) * sizeof(Pergunta));
+     if (lista == NULL) {
+        printf("Erro ao alocar memória!\n");
+        exit(1);
+    }
+
+    lista[*totalPergunta] = nova;
+    (*totalPergunta)++;
+
+    printf("Pergunta inserida");
+   return lista;
+
+}
+int main(){
+    Pergunta* lista = NULL;//incializa com vetor nulo 
+    int totalPergunta = 0;
 
     return 0;
 }
