6f9870c2574b83d2599901fa487a7fa8c054776c
Mayaomoto <mayaomoto@gmail.com>
2025-05-23 10:31:47 -0300

    Lista e insere corrigido

--- test1.c
+++ test1.c
@@ -29,20 +29,21 @@ void leString(char texto[], int tam)
 Pergunta* inserirPergunta(Pergunta* perguntasDoJogo, int* totalPeguntas)
 {
     Pergunta nova;
+   
     printf("Digite o enunciado da pergunta: \n");
-    leString(nova.enunciado,71);
+    leString(nova.enunciado, sizeof(nova.enunciado));
 
     for (int i = 0; i < 4; i++)
     {
         printf("Digite a alternativa %c: " ,'A' + i);//A+i transforma A em 0,1,2,3
-        leString(nova.alternativa_escrita[i],21);
+        leString(nova.alternativa_escrita[i],sizeof(nova.alternativa_escrita[i]));
     }
     printf("Digite a alternativa correta: \n");
     scanf("%c",&nova.alternativa_correta);
     getchar();
 
     printf("Digite o nivel de dificuldade da sua pergunta: \n");
-    leString(nova.nivel_dificuldade,21);
+    leString(nova.nivel_dificuldade,sizeof(nova.nivel_dificuldade));
 
     perguntasDoJogo  = realloc(perguntasDoJogo, (*totalPeguntas + 1) * sizeof(Pergunta));
     if (perguntasDoJogo == NULL)
@@ -60,10 +61,46 @@ Pergunta* inserirPergunta(Pergunta* perguntasDoJogo, int* totalPeguntas)
 
 }
 
+void listaPerguntas(Pergunta perguntas[], int totalPerguntasCad)
+{
 
+
+    if (totalPerguntasCad == 0)
+    {
+       printf("Nenhuma pergunta cadastrada\n");
+       return;
+    }
+    printf("Lista Perguntas Cadastradas (%d): \n",totalPerguntasCad);
+    for (int i = 0; i < totalPerguntasCad; i++)
+    {
+       printf("Pergunta %d : \n",i + 1);
+       printf("Enunciado: %s",perguntas[i].enunciado);
+
+       for (int j = 0; j < 4; j++)
+       {
+            printf("    %c) %s\n", 'A' + j, perguntas[i].alternativa_escrita[j]);
+        
+       }
+    
+       printf("A alternativa correta e: %c\n",perguntas[i].alternativa_correta);
+       printf("Nivel de dificuldade da pergunta: %s",perguntas[i].nivel_dificuldade);
+
+    }
+    
+    return;
+
+
+}
 
 int main(){
-    Pergunta* perguntasDoJogo = NULL;//incializa com vetor nulo 
+   Pergunta* perguntasDoJogo = (Pergunta*) malloc (50 *sizeof(Pergunta));
+    if (perguntasDoJogo == NULL)
+    {
+        printf("Erro ao alocar memoria\n");
+        exit(1);
+    }
+    
+
     int totalPergunta = 0;//contador de perguntas
 
     int opcao;
@@ -72,6 +109,7 @@ int main(){
         printf(" 1 - Inserir pergunta \n");
         printf(" 2 - Listar Perguntas  \n");
         printf(" 0 - Sair \n");
+        printf("Escolha uma opcao: \n");
         scanf("%d",&opcao);
         getchar();
 
@@ -81,9 +119,12 @@ int main(){
             perguntasDoJogo = inserirPergunta(perguntasDoJogo,&totalPergunta);
             break;
         case 2:
-
+            listaPerguntas(perguntasDoJogo,totalPergunta);
             break;
-        default:
+        case 0:
+            printf("Saindo do Jogo...\n");
+            break;
+        default:printf("Opcao invalida, tente novamente !\n");
             break;
         }
     } while (opcao != 0 );
